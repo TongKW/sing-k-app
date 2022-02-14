19479c78734d3e8d3b48f37811be6593afda5ec7
petertong32 <peter@savvyapp.cc>
2022-02-15 00:44:31 +0800

    Tailwind classes refractoring

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import styles from '../styles/Home.module.css'
 import React, { useState, useEffect } from 'react';
 import MobileNavBar from '../component/navigationBar/mobile-nav-bar';
 import Icon from '../component/elements/Icon';
-import { navElemClass } from '../styles/tailwindClasses';
+import { navBarClass, navElemClass } from '../styles/tailwindClasses';
 import logout from '../utils/logout';
 
 export default function Home() {
@@ -15,19 +14,15 @@ export default function Home() {
       sidebar.classList.toggle("-translate-x-full");
     });
     setUsername(localStorage.getItem('username'));
-  })
+  }, [])
   return (
     <div>
-      <div className="relative min-h-screen md:flex">
-        
+      <div className="relative min-h-screen md:flex">       
       <MobileNavBar></MobileNavBar>
-
-
-      <div className="sidebar bg-gray-700 text-blue-100 w-64 space-y-2 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
+      <div className={navBarClass}>
         <a href="#" className="text-white flex items-center space-x-2 px-4">
           <Icon length="50"></Icon>
         </a>
-
         <nav>
           <a href="profile" className={navElemClass}>
             User Profile
@@ -35,10 +30,8 @@ export default function Home() {
           <div className={navElemClass} onClick={logout}>
             Logout
           </div>
-          
         </nav>
       </div>
-
         <div className="flex-1 p-10 text-2xl font-bold">
           welcome, {username}!
         </div>
--- styles/tailwindClasses.js
+++ styles/tailwindClasses.js
@@ -1,6 +1,10 @@
 // This file stores the tailwind classes defined for different ui elements
 // 
 // Side Navigation Bar elements
-export const navElemClass = "block py-2.5 px-4 rounded transition duration-200 hover:bg-indigo-800 hover:text-white cursor-pointer";
+export const navElemClass = "block p-3 rounded transition duration-200 hover:bg-indigo-800 hover:text-white cursor-pointer";
+// Side Navigation Bar
+export const navBarClass = "sidebar bg-gray-700 text-blue-100 w-64 space-y-2 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out";
 // Title text for input form
 export const formTitleClass = "block text-white text-sm font-bold mb-2";
+// Text field for input form
+export const inputFormClass = "text-sm shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
