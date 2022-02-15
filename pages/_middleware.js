e957bfbb45a41505a430bab0dd6cbb230cacbe52
petertong32 <peter@savvyapp.cc>
2022-02-15 18:00:46 +0800

    test

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -8,13 +8,13 @@ import { NextResponse } from 'next/server'
 export function middleware(req) {
   // If from api call, do not redirect to login page
   try {
-    console.log(req.page.name)
+    console.log(`req.page.name: ${req.page.name}`)
     if (req.page.name.startsWith('/api')) return;
   } catch (error) {
     //
   }
 
-  console.log(req.cookies);
+  console.log(`req.cookies: ${req.cookies}`);
   const isLogin = 'username' in req.cookies;
   return NextResponse.rewrite(`/${isLogin ? '' : 'login'}`);
 } 
