c56cb8aa51a34ae810dc8068182890e275b75e63
petertong32 <peter@savvyapp.cc>
2022-02-15 13:43:18 +0800

    Added vercel json for CORS

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -8,6 +8,7 @@ import { NextResponse } from 'next/server'
 export function middleware(req) {
   // If from api call, do not redirect to login page
   try {
+    console.log(req.page.name)
     if (req.page.name.startsWith('/api')) return;
   } catch (error) {
     //
