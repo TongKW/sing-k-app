e2f2ab1148136fa7ad72c6380d14bc03b398b0d7
Katherine <katherinecheng32@gmail.com>
2022-02-26 15:25:38 +0800

    fix styling problem after reload

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,47 +3,40 @@ import HomePage from '../../component/wrapper/HomePage';
 import {
   Box,
   Badge,
-  makeStyles,
   Avatar,
-  withStyles,
   LinearProgress,
   Typography,
-} from '@material-ui/core';
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 
-
-const SmallAvatar = withStyles((theme) => ({
-  root: {
-    width: 22,
-    height: 22,
-    border: `2px solid ${theme.palette.background.paper}`,
-    [theme.breakpoints.up('md')]: {
-      width: 32,
-      height: 32,
-    },
+const SmallAvatar = styled(Avatar)(({ theme }) => ({
+  width: 22,
+  height: 22,
+  border: `2px solid ${theme.palette.background.paper}`,
+  [theme.breakpoints.up('md')]: {
+    width: 32,
+    height: 32,
   },
-}))(Avatar);
+}));
 
-const useStyles = makeStyles((theme) => ({
-  userAvatar: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-    [theme.breakpoints.up('md')]: {
-      width: theme.spacing(17),
-      height: theme.spacing(17),
-    },
+const UserAvatar = styled(Avatar)(({ theme }) => ({
+  width: theme.spacing(9),
+  height: theme.spacing(9),
+  [theme.breakpoints.up('md')]: {
+    width: theme.spacing(17),
+    height: theme.spacing(17),
   },
 }));
 
 
 export default function Profile() {
-  const classes = useStyles();
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   // Avatar in base64 encoding
   const [avatar, setAvatar] = useState();
   const description = "Hello, everyone!";
-  const userExp = 20;
+  const userExp = 10;
   const ExpToNextLevel = 100;
   const bar=Number(userExp)/Number(ExpToNextLevel)*100;
 
@@ -80,7 +73,7 @@ export default function Profile() {
     <HomePage href='profile'>
       <div className="flex-1 p-10 text-2xl font-bold">
       <Box sx={{ display: "flex", flexDirection: "row", alignContent: "center", alignItems: "center"}}>
-        <Box sx={{ display: "flex", flexDirection: "column", width: {xs: "30%", md: "20%"}}}>
+        <Box sx={{ display: "flex", flexDirection: "column", width: {xs: "30%", md: "20%"}, alignContent: "center", alignItems: "center"}}>
           <Box>
             <center>
               <Badge
@@ -91,15 +84,13 @@ export default function Profile() {
                 }}
                 badgeContent={<SmallAvatar src="https://cdn0.iconfinder.com/data/icons/professional-avatar-10/65/38-512.png" />}
               >
-                <Avatar encoding={avatar} className={classes.userAvatar} mx="auto"/>
+                <UserAvatar encoding={avatar} mx="auto"/>
               </Badge>
             </center>
           </Box>
-          <Box width="100%" mr={3} mt={2}>
+          <Box sx={{ width: {xs: "80%", md: "100%"}, mt: {xs:1, md: 2} }} >
             <LinearProgress variant="determinate" value={bar}/>
-          </Box>
-          <Box minWidth={20}>
-            <Typography variant="subtitle2" color="white" ><Box sx={{ fontSize: 10}}>{`${userExp}/${ExpToNextLevel} `}</Box></Typography>
+            <Typography variant="subtitle2" color="white" ><Box sx={{ fontSize: 10, mr: "80%"}}>{`${userExp}/${ExpToNextLevel} `}</Box></Typography>
           </Box>
         </Box>
         <Box ml={3}/>
