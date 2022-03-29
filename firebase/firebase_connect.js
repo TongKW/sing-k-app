b0aad996f0ce55b5b63a08812ea5b5b08dd3c02f
pongpongccc <pongpongccc@gmail.com>
2022-03-29 20:00:17 +0800

    DELETE room

--- firebase/firebase_connect.js
+++ firebase/firebase_connect.js
@@ -1,12 +1,13 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore,collection,getDocs, doc, setDoc  } from "firebase/firestore";
-import { firebaseConfig } from './config.js';
+import {initializeApp} from 'firebase/app';
+import {getFirestore, collection, getDocs, doc, setDoc, deleteDoc } from "firebase/firestore";
+import {firebaseConfig} from './config.js';
+
 initializeApp(firebaseConfig);
 const db = getFirestore();
 
 
 export async function getRoomList() {
-    var querySnapshot  = await getDocs(collection(db,'rooms'));
+    var querySnapshot = await getDocs(collection(db, 'rooms'));
     var result = [];
     querySnapshot.forEach((doc) => {
         result.push(doc.id);
@@ -17,7 +18,7 @@ export async function getRoomList() {
 }
 
 export async function getPublicRoomList() {
-    var querySnapshot  = await getDocs(collection(db,'rooms'));
+    var querySnapshot = await getDocs(collection(db, 'rooms'));
     var result = [];
     querySnapshot.forEach((doc) => {
         if (doc.data()['type'] === 'public') {
@@ -28,7 +29,7 @@ export async function getPublicRoomList() {
 }
 
 export async function getPrivateRoomList() {
-    var querySnapshot  = await getDocs(collection(db,'rooms'));
+    var querySnapshot = await getDocs(collection(db, 'rooms'));
     var result = [];
     querySnapshot.forEach((doc) => {
         if (doc.data()['type'] === 'private') {
@@ -39,29 +40,25 @@ export async function getPrivateRoomList() {
 }
 
 export async function getRoom(roomId) {
-    var rooms = db.collection('/rooms/'+roomId).listCollections();
+    var rooms = db.collection('/rooms/' + roomId).listCollections();
     const roomIds = rooms.map(col => col.id);
-    return { collections: collectionIds };
+    return {collections: collectionIds};
 }
 
 export async function createRoom(roomId, creatorID, type, usersArray) {
     console.log(roomId);
     await setDoc(doc(db, "rooms", roomId), {
-        creator:creatorID,
-        type:type
+        creator: creatorID,
+        type: type
     });
 }
 
-export async function delRoom(roomId) {
-    var ref = db.collection('rooms').doc(roomId);
-
-    ref.delete().then(() => {
-        console.log('delete data successful');
-    });
+export async function delRoom(roomID) {
+    await deleteDoc(doc(db, "rooms", roomID));
 }
 
 export async function delUserFromRoom(roomId, userId) {
-    var ref = db.collection('/rooms/'+roomId+'/').doc(user);
+    var ref = db.collection('/rooms/' + roomId + '/').doc(user);
 
     ref.delete().then(() => {
         console.log('delete data successful');
