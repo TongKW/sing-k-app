cf73e0bd0a9c3d81cef7336675b10d3e2ad8837b
petertong32 <peter@savvyapp.cc>
2022-04-04 01:17:11 +0800

    updated firebase config

--- firebase/config.js
+++ firebase/config.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "sing-k-app.firebaseapp.com",
-  projectId: "sing-k-app",
-  storageBucket: "sing-k-app.appspot.com",
-  messagingSenderId: "241935645461",
-  appId: "1:241935645461:web:ed207a86af7a34c628871c",
-  measurementId: "G-W9VCR551MH"
+  authDomain: "sing-k-app-b4399.firebaseapp.com",
+  projectId: "sing-k-app-b4399",
+  storageBucket: "sing-k-app-b4399.appspot.com",
+  messagingSenderId: "890499235581",
+  appId: "1:890499235581:web:8cb729b1eadbe0b8724894"
 };
