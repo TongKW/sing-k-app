c96e75a688d472a5ce3cdecadca3a141b5124ef3
alexau922 <alexuau922@gmail.com>
2022-04-09 20:45:05 +0800

    Added sorting on username and email for admin

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -11,20 +11,24 @@ import {
   Paper,
   Box,
   IconButton,
+  Image,
 } from "@mui/material";
 import { FormInputBlock } from "../../component/elements/form-input";
 import logout from "../../utils/logout";
 import pwValidateSetError from "../../utils/validate-password-format";
 import { Loading } from "notiflix/build/notiflix-loading-aio";
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import HomePage from "../../component/wrapper/HomePage";
+import sortOnKey from "../../utils/sortOnKey";
+import Icon from "../../component/elements/Icon";
 
 export default function Admin() {
   const router = useRouter();
   const [fetching, setFetching] = useState(true);
-  const [username, setUsername] = useState();
   const [validated, setValidated] = useState(false);
   const [userList, setUserList] = useState();
+  const [usernameDescending, setUsernameDescending] = useState(false);
+  const [emailDescending, setEmailDescending] = useState(false);
   useEffect(() => {
     // validate admin user again by validating the token stored in local storage
     if (fetching) {
@@ -40,7 +44,6 @@ export default function Admin() {
         console.log(data);
         if (data.authorized) {
           const user = data.body;
-          setUsername(user.username);
           if (user.username == "admin") {
             setValidated(true);
           } else {
@@ -127,9 +130,21 @@ export default function Admin() {
     router.push(`/admin/view-profile/${userId}`);
   }
 
-  function handleLogout() {
-    logout();
-  }
+  const updateUI = () => setValue((value) => !value);
+
+  const handleSortUsernameDescending = () => {
+    const newUsernameDescending = !usernameDescending;
+    const newUserList = sortOnKey(userList, "username", newUsernameDescending);
+    setUsernameDescending(newUsernameDescending);
+    setUserList(newUserList);
+  };
+
+  const handleSortEmailDescending = () => {
+    const newEmailDescending = !emailDescending;
+    const newUserList = sortOnKey(userList, "email", newEmailDescending);
+    setEmailDescending(newEmailDescending);
+    setUserList(newUserList);
+  };
 
   if (!validated) {
     return <div>Validating... </div>;
@@ -140,40 +155,116 @@ export default function Admin() {
 
     return (
       <>
-      {fetching ? (
+        {fetching ? (
           <div>Validating...</div>
         ) : (
-      <HomePage role={"admin"}>
-        <TableContainer
-          component={Paper}
-          sx={{ width: {xs: "80%", sm:"70%"}, margin: "auto", mt: 5 }}
-        >
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Username</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>New password</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {userList &&
-                userList.map((userObj, index) => (
-                  <ListUser
-                    key={index}
-                    username={userObj.username}
-                    email={userObj.email}
-                    userId={userObj.userId}
-                    handleChangePw={handleChangePw}
-                    handleViewProfile={handleViewProfile}
-                  />
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </HomePage>
-) }
-</>
+          <HomePage role={"admin"}>
+            <TableContainer
+              component={Paper}
+              sx={{ width: { xs: "80%", sm: "70%" }, margin: "auto", mt: 5 }}
+            >
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      onClick={handleSortUsernameDescending}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <Box
+                        style={{
+                          display: "flex",
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                          alignItems: "center",
+                          width: "80%",
+                        }}
+                      >
+                        Username
+                        {usernameDescending ? (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-sort-up-alt"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707V13.5zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1h-1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1h-3zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1h-5zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5z" />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-sort-down"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
+                          </svg>
+                        )}
+                      </Box>
+                    </TableCell>
+                    <TableCell
+                      onClick={handleSortEmailDescending}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <Box
+                        style={{
+                          display: "flex",
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                          alignItems: "center",
+                          width: "60%",
+                        }}
+                      >
+                        Email
+                        {emailDescending ? (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-sort-up-alt"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M3.5 13.5a.5.5 0 0 1-1 0V4.707L1.354 5.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 4.707V13.5zm4-9.5a.5.5 0 0 1 0-1h1a.5.5 0 0 1 0 1h-1zm0 3a.5.5 0 0 1 0-1h3a.5.5 0 0 1 0 1h-3zm0 3a.5.5 0 0 1 0-1h5a.5.5 0 0 1 0 1h-5zM7 12.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7a.5.5 0 0 0-.5.5z" />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-sort-down"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293V2.5zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z" />
+                          </svg>
+                        )}
+                      </Box>
+                    </TableCell>
+                    <TableCell>New password</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {userList &&
+                    userList.map((userObj, index) => (
+                      <ListUser
+                        key={index}
+                        username={userObj.username}
+                        email={userObj.email}
+                        userId={userObj._id}
+                        handleChangePw={handleChangePw}
+                        handleViewProfile={handleViewProfile}
+                      />
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </HomePage>
+        )}
+      </>
     );
   }
 }
